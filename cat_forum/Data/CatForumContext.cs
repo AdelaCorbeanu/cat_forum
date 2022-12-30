4613b9831681788768ea6182ce6d82c8f528ed80
AdelaCorbeanu <92207857+AdelaCorbeanu@users.noreply.github.com>
2022-12-30 14:56:45 +0200

    beginning

---
+++ cat_forum/Data/CatForumContext.cs
@@ -0,0 +1,15 @@
+﻿using cat_forum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cat_forum.Data
+{
+    public class CatForumContext : DbContext
+    {
+        public DbSet<ForumPost> ForumPosts { get; set; }
+        public CatForumContext(DbContextOptions<CatForumContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ cat_forum/Models/ForumPost.cs
@@ -0,0 +1,11 @@
+﻿using cat_forum.Models.Base;
+
+namespace cat_forum.Models
+{
+    public class ForumPost : BaseEntity
+    {
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
